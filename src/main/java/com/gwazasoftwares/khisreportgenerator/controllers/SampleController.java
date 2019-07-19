6f60d9f9b6c45b4cf9efe8bb6a0858f8e813bb99
AustineGwa <austinegwa64@gmail.com>
2019-07-19 13:53:22 +0300

    Updated loading screen and added second screen / stage

--- src/main/java/com/gwazasoftwares/khisreportgenerator/controllers/SampleController.java
+++ src/main/java/com/gwazasoftwares/khisreportgenerator/controllers/SampleController.java
@@ -1,9 +1,11 @@
 package com.gwazasoftwares.khisreportgenerator.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import org.springframework.stereotype.Service;
 
@@ -12,22 +14,33 @@ import javax.swing.*;
 @Service
 public class SampleController {
 
-    private Parent rootNode;
-    private FXMLLoader fxmlLoader;
+    @FXML
+    Button btnNext;
 
 
     public void onLoginButtonClicked() throws Exception{
 
-        fxmlLoader = new FXMLLoader();
+        FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getResource("/fxml/firstSemesterChart.fxml"));
-        rootNode = fxmlLoader.load();
+        Parent rootNode = fxmlLoader.load();
 
         Stage stage = new Stage();
         stage.setTitle("My New Stage Title");
         stage.setScene(new Scene(rootNode, 450, 450));
+        stage.setFullScreen(true);
         stage.show();
         // Hide this current window (if this is what you want)
-        //((Node)(event.getSource())).getScene().getWindow().hide();
+        hideCurrentScreen();
 
+
+
+    }
+
+    private void hideCurrentScreen() {
+
+        // get a handle to the stage
+        Stage stage = (Stage) btnNext.getScene().getWindow();
+        // do what you have to do
+        stage.close();
     }
 }
